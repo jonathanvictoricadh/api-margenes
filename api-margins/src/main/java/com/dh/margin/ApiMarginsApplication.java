c096014d9f6250c8e003060a88ccb4b76db21acf
jonathanvictorica <jonathan@techforb.com>
2023-02-21 01:28:58 -0300

    init

---
+++ api-margins/src/main/java/com/dh/margin/ApiMarginsApplication.java
@@ -0,0 +1,13 @@
+package com.dh.margin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMarginsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMarginsApplication.class, args);
+	}
+
+}
